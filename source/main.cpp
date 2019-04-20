9ee26cd6dda2e95853f6c36e017d3f2d353f1f9b
Charles "CJ" Emerson <cjemerson@alaska.edu>
2019-04-19 22:14:19 -0800

    Add: Circles Drawn when User Clicks

--- source/main.cpp
+++ source/main.cpp
@@ -18,6 +18,11 @@ int main()
     auto bounds = sf::FloatRect(0.0, 0.0, 800.0, 600.0);
     auto axes = Axes(bounds, 2.0, sf::Color::White);
 
+    // Circles drawn when user clicks
+    const float CIRCLE_RADIUS = 5.0; 
+    const auto CIRCLE_COLOR = sf::Color::Red;
+    auto circles = std::vector<sf::CircleShape>{};
+
     // Start the game loop
     while (window.isOpen())
     {
@@ -28,7 +33,22 @@ int main()
         {
             // Close window: exit
             if (event.type == sf::Event::Closed)
+            {
                 window.close();
+            }
+            else if (event.type == sf::Event::MouseButtonPressed)
+            {
+                if (event.mouseButton.button == sf::Mouse::Button::Left)
+                {
+                    auto x = event.mouseButton.x;
+                    auto y = event.mouseButton.y;
+
+                    auto circle = sf::CircleShape(CIRCLE_RADIUS);
+                    circle.setPosition(float(x) - CIRCLE_RADIUS, float(y) - CIRCLE_RADIUS);
+                    circle.setFillColor(CIRCLE_COLOR);
+                    circles.push_back(circle);
+                }
+            }
         }
 
         // Clear screen
@@ -36,6 +56,12 @@ int main()
 
         // Draw Axes
         window.draw(axes);
+
+        // Draw Circles
+        for (const auto & circle : circles)
+        {
+            window.draw(circle);
+        }
 
         // Update the window
         window.display();
