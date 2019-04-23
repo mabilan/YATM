4c3b7656e3cdee4a95e69c5ecd1c6ec4fe3ecc6c
Charles "CJ" Emerson <cjemerson@alaska.edu>
2019-04-22 18:58:25 -0800

    Add: ImGui + SFML Main
     - Added AppMode enum
     - Added Default Category definition
     - Removed use of Graph class and replaced with TaskGrid class
       (NET: Can no longer remove a task by calling the owning object)
     - Added "Delete Prompt" Window
     - Added "Task Details" Window
     - Added MenuBar with MenuItem "Help" and Help Window

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,7 @@
 // main.cpp
-//
-// Simple SFML main.
 
+#include <iostream>
+#include <memory>
 
 #include "imgui.h"
 #include "imgui-SFML.h"
@@ -9,32 +9,73 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
-#include "graphics.hpp"
+#include "category.hpp"
+#include "task.hpp"
+#include "task_grid.hpp"
+
+enum class AppMode
+{
+    TASK_GRID_VIEW, // View Tasks on a Grid
+    CALENDAR_VIEW,  // View Tasks on a Calendar @TODO
+    NEW_TASK,       // Place Task on Grid
+    TASK_DETAILS,   // Update task meta data 
+    ADD_CATEGORY,   // Create new category @TODO
+    DELETE_PROMPT   // Prompt for Delete Task
+};
+
+// Unable to define Default Category in Category.cpp
+void defineDefaultCategory()
+{
+    Category::Default = std::make_shared<Category>();
+    Category::Default->setName("Default");
+
+    auto unfocused = std::make_unique<sf::CircleShape>(5.0);
+    unfocused->setFillColor(sf::Color::Red);
+    unfocused->setPointCount(3);
+    Category::Default->setUnfocusedDrawable(std::move(unfocused));
+
+    auto focused = std::make_unique<sf::CircleShape>(5.0);
+    focused->setFillColor(sf::Color::Red);
+    focused->setPointCount(3);
+    focused->setOutlineColor(sf::Color::Yellow);
+    focused->setOutlineThickness(2.0);
+    Category::Default->setFocusedDrawable(std::move(focused));
+}
 
 
 int main()
 {
+    // ****************************************************************
+    // APP INITIALIZATION
+    // ****************************************************************
     // Create the main window
     sf::RenderWindow window(sf::VideoMode(800, 600), "YATM: Yet Another Task Manager");
     window.setVerticalSyncEnabled(true);
     ImGui::SFML::Init(window);
 
-    // sf::FloatRect(left, top, width, height)
+    // App Mode
+    AppMode mode = AppMode::TASK_GRID_VIEW;
+    bool displayHelpWindow = false;
+    std::cout << "TASK_GRID_VIEW" << std::endl;
+
+    // Define Default Category
+    defineDefaultCategory();
+
+    // @TODO - Load existing task list from file 
+    std::vector<Task> tasklist{};
+    auto selected = tasklist.end(); // <- Used for focusing tasks
+
+    // Setup TaskGrid
     auto bounds = sf::FloatRect(0.0, 0.0, 600.0, 600.0);
-    auto axes = Axes(bounds, 2.0, sf::Color::White);
-
-    // Template Circle to be used within Graph
-    const float CIRCLE_RADIUS = 5.0;
-    const auto CIRCLE_COLOR = sf::Color::Red;
-    auto circle = sf::CircleShape(CIRCLE_RADIUS);
-    circle.setFillColor(CIRCLE_COLOR);
-    circle.setPointCount(3); // <-- Makes it look like a triangle
-
-    // Graph
-    auto graph = Graph(axes, circle);
+    TaskGrid grid = TaskGrid(bounds);
 
     // Used by ImGui
     sf::Clock deltaClock;
+
+
+    // ****************************************************************
+    // GAME LOOP
+    // ****************************************************************
 
     // Start the game loop
     while (window.isOpen())
@@ -52,59 +93,204 @@ int main()
             {
                 window.close();
             }
+            // Resize window event
+            else if (event.type == sf::Event::Resized)
+            {
+                // @TODO - Need to update View and ImGui Widgets Size
+            }
+            else if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::Escape)
+                {
+                    mode = AppMode::TASK_GRID_VIEW;
+                    std::cout << "TASK_GRID_VIEW" << std::endl;
+                }
+                else if (event.key.code == sf::Keyboard::F1)
+                {
+                    // Toggle Display Help Window
+                    displayHelpWindow = !displayHelpWindow;
+                }
+                else if (event.key.code == sf::Keyboard::Q)
+                {
+                    mode = AppMode::NEW_TASK;
+                    std::cout << "NEW_TASK" << std::endl;
+                }
+                else if (event.key.code == sf::Keyboard::Delete)
+                {
+                    mode = AppMode::DELETE_PROMPT;
+                    std::cout << "DELETE_PROMPT" << std::endl;
+                }
+                else if (event.key.code == sf::Keyboard::W)
+                {
+                    mode = AppMode::TASK_DETAILS;
+                    std::cout << "TASK_DETAILS" << std::endl;
+                }
+            }
             // Mouse Click
             else if (event.type == sf::Event::MouseButtonPressed)
             {
                 auto x = event.mouseButton.x;
                 auto y = event.mouseButton.y;
 
-                // Mouse Click - Left Button
                 if (event.mouseButton.button == sf::Mouse::Button::Left)
                 {
-                    graph.addCircle(x,y);
-                }
-                // Mouse Click - Right Button
-                else if (event.mouseButton.button == sf::Mouse::Button::Right)
-                {
-                    graph.removeCircle(x, y);
+                    // If mouse click is within bounds
+                    auto isOnGrid = grid.contains(x, y);
+                    if (isOnGrid)
+                    {
+                        if (mode == AppMode::NEW_TASK)
+                        {
+                            tasklist.emplace_back();
+                            auto urgVsImp = grid.getUrgVsImp(x, y);
+
+                            // selected points to the newly created element
+                            selected =  std::prev(tasklist.end());;
+
+                            selected->setUrgency(urgVsImp.x);
+                            selected->setImportance(urgVsImp.y);
+                        }
+                        else
+                        {
+                            // @TODO - Select a task if close enough
+                        }
+                    }
                 }
             }
         }
 
-        // *** ImGui Widget for Task Stack
 
         // Imgui Update() Required
         ImGui::SFML::Update(window, deltaClock.restart());
 
+
+        // ************************************************************
+        // ImGui Windows and Such
+        // ************************************************************
+
+        // *** Menu bar
+        if (ImGui::BeginMainMenuBar())
+        {
+            if (ImGui::MenuItem("Help", "F1", displayHelpWindow))
+            {
+                displayHelpWindow = true;
+            }
+            ImGui::EndMainMenuBar();
+
+        }
+        if (displayHelpWindow)
+        {
+            ImGui::SetNextWindowBgAlpha(0.9f); // Transparent background
+            if (ImGui::Begin("Help", &displayHelpWindow, ImGuiWindowFlags_NoCollapse))
+            {
+                ImGui::TextUnformatted("F1 - Display this message\nQ - Enter 'Insert Task Mode'\nW - Look at selected task's details\nDelete - Delete selected task\nEscape - Enter 'Task Grid View'");
+                ImGui::End();
+            }
+            else
+            {
+                displayHelpWindow = false;
+            }
+        }
+
+
+        // *** The following ImGui windows need a nonempty tasklist and focused task
+
+        if (tasklist.empty() || selected == tasklist.end())
+        {
+            // Do nothing
+        }
+        else if (mode == AppMode::DELETE_PROMPT)
+        {
+            ImGui::SetNextWindowPos(ImVec2(200, 200), ImGuiCond_FirstUseEver);
+            ImGui::SetNextWindowBgAlpha(0.9f); // Transparent background
+            ImGui::Begin("Delete Prompt", 0, ImGuiWindowFlags_NoResize);
+            ImGui::Text("Are you sure you want to delete the task: ");
+
+            ImGui::PushID(&selected);
+            auto name = selected->getName();
+
+            // TextUnformatted(char * begin, char * end)
+            ImGui::TextUnformatted(name.c_str(), name.c_str() + name.size());
+            ImGui::PopID();
+
+            if(ImGui::Button("No"))
+            {
+                mode = AppMode::TASK_GRID_VIEW;
+                std::cout << "TASK_GRID_VIEW" << std::endl;
+            }
+            ImGui::SameLine();
+            if(ImGui::Button("Yes"))
+            {
+                tasklist.erase(selected);
+
+                // selected points to end (so no focus)
+                selected = tasklist.end();
+                mode = AppMode::TASK_GRID_VIEW;
+                std::cout << "TASK_GRID_VIEW" << std::endl;
+            }
+            ImGui::End(); // End "Delete Prompt"
+        }
+        else if (mode == AppMode::TASK_DETAILS)
+        {
+            ImGui::SetNextWindowPos(ImVec2(200, 200), ImGuiCond_FirstUseEver);
+            ImGui::SetNextWindowBgAlpha(0.9f); // Transparent background
+            ImGui::Begin("Task Details");
+
+            auto id = selected->getId();
+            auto name = selected->getName();
+            auto importance = selected->getImportance();
+            auto urgency = selected->getUrgency();
+
+            ImGui::Value("Id", id);
+
+            char buffer[256];
+            strcpy(buffer, name.c_str()); // copy name to buffer
+            if (ImGui::InputText("Name", buffer, 256, ImGuiInputTextFlags_EnterReturnsTrue))
+            {
+                selected->setName(std::string(buffer));
+            }
+
+            if (ImGui::SliderFloat("Importance", &importance, Task::MIN_IMPORTANCE, Task::MAX_IMPORTANCE))
+            {
+                selected->setImportance(importance);
+            }
+
+            if (ImGui::SliderFloat("Urgency", &urgency, Task::MIN_URGENCY, Task::MAX_URGENCY))
+            {
+                selected->setUrgency(urgency);
+            }
+            ImGui::End(); // End "Task Details"
+        }
+
+        // *** ImGui Widget for Task Stack
         // ImGuiCond_Always forces these conditions
         ImGui::SetNextWindowSize(ImVec2(200, 600), ImGuiCond_Always);
         ImGui::SetNextWindowPos(ImVec2(600, 0), ImGuiCond_Always);
         ImGui::SetNextWindowBgAlpha(0.35f); // Transparent background
         ImGui::Begin("Task Stack", 0, ImGuiWindowFlags_NoResize);
 
-        static int selected = 0;
-        const auto & positions = graph.getPositions();
-        for (int i = 0; i < int(positions.size()); ++i)
+        for (auto iter = tasklist.begin(); iter != tasklist.end(); ++iter)
         {
-            // This is temporary.
-            // Ideally, get C style strings from Michael Bilan's scheduler
+            auto name = iter->getName();
+            auto id = iter->getId();
 
-            char label[128];
-            sprintf(label, "Circle at (%d, %d)", int(positions[i].x), int(positions[i].y));
-            if (ImGui::Selectable(label, selected == i))
+            name = name + "##" + std::to_string(id);
+            if (ImGui::Selectable(name.c_str(), iter == selected))
             {
-                selected = i;
-                graph.setFocus(selected);
+                selected = iter;
             }
         }
-        ImGui::End(); // End ImGui widget window
+        ImGui::End(); // End "Task Stack"
 
+
+        // ************************************************************
+        // RENDER CALLS
+        // ************************************************************
 
         // Clear screen
         window.clear();
 
         // Draw Graph
-        window.draw(graph);
+        grid.draw(window, tasklist, selected);
 
         // Render ImGui widgets on top of everything else
         ImGui::SFML::Render(window);
@@ -112,6 +298,11 @@ int main()
         // Update the window
         window.display();
     }
+
+
+    // ****************************************************************
+    // APP SHUTDOWN
+    // ****************************************************************
 
     // CJ: My understanding is ShutDown() does some clean up
     ImGui::SFML::Shutdown();
