038b27290ffe790306531cdb0c889dc1369d863b
arparker5 <arparker5@alaska.edu>
2019-04-30 16:11:33 -0800

    Implementing datetime and time classes

--- include/datetime.hpp
+++ include/datetime.hpp
@@ -7,28 +7,38 @@
 
 #include <vector>
 
+class Time{
+public:
+    Time(int month, int date, int year);
+    ~Time() = default;
+
+    void setTime(int month, int date, int year);
+    std::vector<int> getTime();
+private:
+    int _month;
+    int _date;
+    int _year;
+};
+
+
 class DateTime{
 public:
     DateTime();
 
-    DateTime(int month, int date, int year, int hour, int minute, bool am);
+    DateTime(int month, int date, int year);
     ~DateTime() = default;
 
-    void setDateTime(int month, int date, int year, int hour, int minute, bool am);
+    void setDateTime(int month, int date, int year);
+    void currentTime();
     std::vector<int> getDateTime();
-
-
+    void addDateTime(Time time);
 
 private:
-
     int _month;
     int _date;
     int _year;
-    int _hour;
-    int _minute;
-    bool _am;     // True: AM, False: PM
+    std::vector<int> _daysinmonths {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 };
-
 
 
 #endif //YATM_DATETIME_H
--- source/datetime.cpp
+++ source/datetime.cpp
@@ -4,29 +4,85 @@
 #include "datetime.hpp"
 #include <iostream>
 #include <string>
-#include <chrono>
 #include <ctime>
+#include <tuple>
 
 using std::string;
 using std::endl;
 using std::cout;
 
 
-DateTime::DateTime(){
+Time::Time(int month, int date, int year): _month(month), _date(date), _year(year){}
 
-}
-
-DateTime::DateTime(int month, int date, int year, int hour, int minute, bool am)
-        : _month(month), _date(date), _year(year), _hour(hour), _am(am){}
-
-void DateTime::setDateTime(int month, int date, int year, int hour, int minute, bool am){
+void Time::setTime(int month, int date, int year){
     _month = month;
     _date = date;
     _year = year;
-    _hour = hour;
-    _am = am;
+}
+
+std::vector<int> Time::getTime(){
+    return std::vector<int> {_month, _date, _year};
+}
+
+
+
+
+void DateTime::currentTime(){
+    struct tm *currtime;
+    std::time_t tim;
+    time(&tim);
+    currtime = localtime(&tim);
+
+    _date = currtime->tm_mday;
+    _month = currtime->tm_mon + 1;
+    _year = currtime->tm_year + 1900;
+}
+
+DateTime::DateTime(int month, int date, int year)
+        : _month(month), _date(date), _year(year){}
+
+void DateTime::setDateTime(int month, int date, int year){
+    _month = month;
+    _date = date;
+    _year = year;
 }
 
 std::vector<int> DateTime::getDateTime(){
-    return std::vector<int> {_month, _date, _year, _hour, _am};
+    return std::vector<int> {_month, _date, _year};
 }
+
+
+void DateTime::addDateTime(Time time){
+    std::vector<int> dt = time.getTime();
+
+    _date += dt[1];
+
+    if(_date > _daysinmonths[_month-1]){
+        _date -= _daysinmonths[_month-1];
+        _month++;
+    }
+
+    _month += dt[0];
+    _year += dt[2];
+
+    if(_month > 12){
+        int overflow = _month/12;
+        _month -= 12*overflow;
+        _year += overflow;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
