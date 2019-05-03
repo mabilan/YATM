f949df1f97db7825dd2782d6fcd6026bdba5dd3f
Michael Bilan <mabilan@alaska.edu>
2019-05-03 08:35:51 -0800

    Add: define adjustSchedule

--- source/scheduler.cpp
+++ source/scheduler.cpp
@@ -2,5 +2,23 @@
 
 #include "scheduler.hpp"
 
+std::vector<Task> adjustSchedule(std::vector<Task> const &tasklist)
+{
+std::vector<std::pair<float, Task>> taskSchedule;
+std::vector<Task> finalSchedule;
 
+for(auto task : tasklist)
+{
+taskSchedule.push_back(std::make_pair(getPriority(task), task));
+}
+
+std::sort(taskSchedule.begin(), taskSchedule.end());
+
+for(auto task : taskSchedule)
+{
+finalSchedule.push_back(task.second);
+}
+
+return finalSchedule;
+}
 
